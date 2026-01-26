2a4c4c6d2bf91eb810457a4b0c8e5e220e9a672a
Naomi Quinones <52065567+naomiquinones@users.noreply.github.com>
2026-01-26 19:49:13 +0000

    Add precedence file

---
+++ precedence.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "12 + 6 / 3 = " << 12 + 6/3 << endl;
+    cout << "(12 + 6) / 3 = " << (12 + 6) / 3 << endl;
+
+    cout << "10 + 20 + 30 / 3 = " << 10 + 20 + 30 / 3 << endl;
+    cout << "(10 + 20 + 30) / 3 = " << (10 + 20 + 30) / 3 << endl;
+
+    return 0;
+}
